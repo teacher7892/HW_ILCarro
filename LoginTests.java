11f8860b10094a76bdf35566fda6c0586e611d60
teacher7892 <107072989+teacher7892@users.noreply.github.com>
2022-06-24 18:16:41 +0400

    Add files via upload

---
+++ LoginTests.java
@@ -0,0 +1,38 @@
+package tests;
+//import org.openqa.selenium.WebDriver;
+
+
+import com.google.common.annotations.VisibleForTesting;
+import org.testng.annotations.Test;
+
+public class LoginTests extends BaseTests {
+    /////success:
+    //1.open browser
+    //2.open www form
+    //3.click on header button 'Login'
+    //4.fill form + valid data
+    //5.submit by click on 'yalla'
+    //6.assert -is login successful?
+    //7.wd quit
+
+    ////unsuccess:
+    //1.open browser
+    //2.open www form
+    //3.click on header button 'Login'
+    //4.fill form + valid data
+    //5.submit by click on 'yalla'
+    //6.assert -is login successful?
+    //7.wd quit
+    @Test
+    public void successLogin (){
+        openLoginForm ();//2,3
+        fillLoginForm ("felicita92@mail.com","Felicita92$");//4 + valid data
+        submitLogin ();//5
+    }
+   @Test
+    public void unsuccessLoginWrongEmail (){
+        openLoginForm ();//2,3
+        fillLoginForm ("felicita92mail.com","Felicita92$");//4 + invalid data
+        submitLogin ();//5
+    }
+}
